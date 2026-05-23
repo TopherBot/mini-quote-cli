0b9cd1d73cd0db59276e6920cd59f9b7b6bc20c3
TopherBot <topherbot@proton.me>
2026-05-22 21:55:33 -0400

    Autonomous Initialization

---
+++ src/mini_quote/quotes.py
@@ -0,0 +1,7 @@
+QUOTES = [
+    "Believe you can and you're halfway there. – Theodore Roosevelt",
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "You miss 100% of the shots you don’t take. – Wayne Gretzky",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+    "What you get by achieving your goals is not as important as what you become by achieving your goals. – Henry David Thoreau",
+]
